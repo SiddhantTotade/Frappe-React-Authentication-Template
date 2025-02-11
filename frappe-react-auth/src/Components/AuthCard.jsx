dcc5d4f6489bd196f2316fbd732f75d1f69e6629
SiddhantTotade <siddhanttotade.1994@gmail.com>
2025-02-11 08:56:31 +0530

    add : AuthCard and Login Page

--- frappe-react-auth/src/Components/AuthCard.jsx
+++ frappe-react-auth/src/Components/AuthCard.jsx
@@ -1,3 +1,18 @@
-export const AuthCard = () => {
-  return <div>Hello</div>;
+export const AuthCard = ({ children }) => {
+  return (
+    <div
+      style={{
+        width: "15%",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+        padding: "10px",
+        border: "2px solid black",
+        boxShadow: "5px 5px",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
--- frappe-react-auth/src/Pages/Login.jsx
+++ frappe-react-auth/src/Pages/Login.jsx
@@ -1,3 +1,68 @@
+import { useState } from "react";
+import { AuthCard } from "../Components/AuthCard";
+import { login } from "../API's/authApi";
+
 export const Login = () => {
-  return <div>Login Page</div>;
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShwoPassword] = useState(true);
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <AuthCard>
+      <div>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+          onSubmit={() => login(credentials.email, credentials.password)}
+        >
+          <input
+            name="email"
+            placeholder="Email"
+            type="email"
+            pattern="/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+            value={credentials.email}
+            required
+            onChange={handleChange}
+            style={{ padding: "10px", outline: "none" }}
+          />
+          <input
+            name="password"
+            placeholder="Password"
+            type={`${showPassword ? "password" : "text"}`}
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+            value={credentials.password}
+            required
+            onChange={handleChange}
+            style={{ padding: "10px", outline: "none" }}
+          />
+          <input
+            type="checkbox"
+            onClick={() => setShwoPassword((showPassword) => !showPassword)}
+            style={{ alignSelf: "end", accentColor: "black" }}
+          />
+          <button
+            type="submit"
+            style={{
+              background: "#000000",
+              color: "#ffffff",
+              outline: "none",
+              border: 0,
+              padding: "10px",
+            }}
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </AuthCard>
+  );
 };
